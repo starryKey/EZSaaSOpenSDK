58c5974a001c7d0e0398274e20578d5f3760441b
typedefine <zhang_xiu_feng@yeah.net>
2022-02-09 17:08:03 +0800

    EZSaaSOpenSDK第一个版本

---
+++ EZSaaSOpenSDK.framework/Headers/EZSaaSOpenSDK.h
@@ -0,0 +1,27 @@
+//
+//  EZSaaSOpenSDK.h
+//  EZSaaSOpenSDK
+//
+//  Created by saas on 2022/1/10.
+//
+
+#import <UIKit/UIKit.h>
+#import "EZSaaSNaviController.h"
+#import "EZSaaSOpenSDKConfig.h"
+
+//! Project version number for EZSaaSOpenSDK.
+FOUNDATION_EXPORT double EZSaaSOpenSDKVersionNumber;
+
+//! Project version string for EZSaaSOpenSDK.
+FOUNDATION_EXPORT const unsigned char EZSaaSOpenSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EZSaaSOpenSDK/PublicHeader.h>
+
+
+@interface EZSaaSOpenSDK : NSObject
+
++ (void)initSDKWithToken:(NSString *)token;
+
++ (UIViewController *)deviceManageController;
+
+@end
--- EZSaaSOpenSDK.podspec
+++ EZSaaSOpenSDK.podspec
@@ -31,12 +31,15 @@ TODO: Add long description of the pod here.
   s.ios.deployment_target = '9.0'
 
   s.source_files = 'EZSaaSOpenSDK/Classes/**/*'
+  s.resource  = "resource/*.bundle"
   
   # s.resource_bundles = {
   #   'EZSaaSOpenSDK' => ['EZSaaSOpenSDK/Assets/*.png']
   # }
 
   # s.public_header_files = 'Pod/Classes/**/*.h'
-  # s.frameworks = 'UIKit', 'MapKit'
-  # s.dependency 'AFNetworking', '~> 2.3'
+  s.frameworks = 'UIKit', 'MapKit'
+  s.dependency 'AFNetworking'
+  s.vendored_frameworks = 'EZSaaSOpenSDK.framework'
+  
 end
